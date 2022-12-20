cb6a7d905f55ea8ad75ef01d9ea692a672e2c83f
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2022-12-20 22:26:59 +0530

    "setting up acios headers"

--- src/features/api/axios.js
+++ src/features/api/axios.js
@@ -4,5 +4,7 @@ export default axios.create({
   baseURL: "https://bestbuy-server.vercel.app/",
   headers: {
     "Access-Control-Allow-Origin": "https://bestbuy-server.vercel.app/",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
 });
