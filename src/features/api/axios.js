41c0de9faa018a95898f209180cb738b0b2472b5
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2022-12-20 22:55:22 +0530

    "change inheaders"

--- src/features/api/axios.js
+++ src/features/api/axios.js
@@ -3,7 +3,10 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://bestbuy-server.vercel.app/",
   headers: {
-    "Access-Control-Allow-Origin": "https://bestbuy-server.vercel.app/",
+    "Access-Control-Allow-Origin": [
+      "https://bestbuy-server.vercel.app/",
+      "http://localhost:3000/",
+    ],
     "Access-Control-Allow-Methods": "GET, POST,PATCH, PUT, DELETE",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
