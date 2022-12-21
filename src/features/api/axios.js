0719072cdd929c6df49d9946a3b940d0926f0053
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2022-12-21 21:28:43 +0530

    "corrected cors after deployment"

--- src/features/api/axios.js
+++ src/features/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://bestbuy-server.vercel.app/",
   headers: {
-    "Access-Control-Allow-Origin": "https://bestbuy-client.vercel.app/",
+    "Access-Control-Allow-Origin": "https://bestbuy-client.vercel.app",
     // "Access-Control-Allow-Origin": "http://localhost:3000",
     "Access-Control-Allow-Methods": "GET, POST,PATCH, PUT, DELETE",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
